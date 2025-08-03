73f54c4787a2a2533985f2fa05afdaf3073c36ca
Haocheng <luhaocheng05@gmail.com>
2025-08-03 14:14:13 +1000

    Implemented create new meeting endpoint with meetingInput interface

--- marketbot/src/backend/api/meeting.ts
+++
@@ -1,7 +0,0 @@
-import { Router, Request, Response } from 'express';
-import { PrismaClient} from '@prisma/client';
-import { ListingInput } from '../interfaces';
-
-const router = Router();
-const prisma = new PrismaClient();
-
--- marketbot/src/backend/interfaces.ts
+++ marketbot/src/backend/interfaces.ts
@@ -12,3 +12,12 @@ export interface ListingInput {
     userId: string;
     status: string;
 }
+
+export interface meetingInput {
+    title: string;
+    description: string;
+    meetingTime: number;
+    meetingLocation: string; 
+    userId: string;
+    status: string; 
+}
